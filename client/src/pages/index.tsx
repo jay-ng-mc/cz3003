eb87b3a0119401bd33e518a043abd0e94e044be4
Jay <milkweedz@hotmail.com>
2021-02-23 15:11:44 +0800

    client environment setup

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Container } from '../components/Container'
+
+const Index = () => (
+  <Container height="100vh">
+    hello world
+  </Container>
+)
+
+export default Index
