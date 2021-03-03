a5d8563a5a5adbdc3bd19f5bebfcf6341f323a3a
behweifeng97 <behweifeng97@gmail.com>
2021-03-03 13:35:34 +0800

    added home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,10 +8,12 @@ import {
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Container } from '../components/Container'
+//import { DiceRoller } from '../components/DiceRoller'
+import { Home } from '../components/Home'
 
 const Index = () => (
   <Container height="100vh">
-    hello world
+    <Home />
   </Container>
 )
 
