4cf9b973b7b636f58aca8a92b2d958fc2c5fc8f3
behweifeng97 <behweifeng97@gmail.com>
2021-03-05 04:05:15 +0800

    choose character layout

---
+++ client/src/components/ChooseCharacter.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {ThemeProvider, theme, CSSReset, Flex, Box, Heading, IconButton, IconButtonProps, Link, FormControl, FormLabel, Input, Stack, Checkbox, Button, Editable, EditableInput, EditablePreview, ButtonGroup, Image, Avatar } from "@chakra-ui/react";
+import { EditIcon, CheckIcon, CloseIcon, ChevronLeftIcon, ChevronRightIcon, ArrowBackIcon } from '@chakra-ui/icons'
+
+let imageList = ["https://bit.ly/sage-adebayo", "https://upload.wikimedia.org/wikipedia/commons/c/c5/Number-One.JPG"]
+var savedImageId = 1
+export const ChooseCharacter = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CSSReset />
+            <CharacterPage />
+        </ThemeProvider>
+    );
+}
+
+const CharacterPage = () => {
+    return (
+        <Flex minHeight='100vh' width='full' align='center' justifyContent='center'>
+            <Box borderWidth={1} px={4} width='full' maxWidth='300px' borderRadius={4} textAlign='center' boxShadow='lg'>
+                <ThemeProvider theme={theme} />
+                <Box p={4}>
+                    <Title />
+                    <CharacterName />
+                    <ChangeAvatar />
+                    <SaveBox />
+                </Box>
+            </Box>
+        </Flex>
+    );
+}
+
+const Title = () => {
+    return (
+        <Box my={1} textAlign='center'>
+        <IconButton 
+        icon={<ArrowBackIcon />}
+        aria-label="Back Icon"
+        isRound={true} 
+        size='sm'/>
+        Choose your character
+        </Box>
+    )
+}
+
+
+const CharacterName = () => {
+    return (
+        <Box textAlign='center'>
+            <Editable
+      textAlign="center"
+      defaultValue="Player 1"
+      fontSize="2xl"
+      isPreviewFocusable={false}
+      submitOnBlur={false}
+    >
+      {(props) => (
+        <>
+          <EditablePreview />
+          <EditableInput />
+          <EditableControls {...props} />
+        </>
+      )}
+    </Editable>
+        </Box>
+    )
+}
+
+const ChangeAvatar = () => {
+    return (
+        <Box my={5} textAlign='center'>
+            <ChooseImage imageId={savedImageId} />
+        </Box>
+    )
+}
+
+const SaveBox = () => {
+    return (
+        <Box my={5} textAlign='left'>
+            
+            <Button width='full' mt={5}>Save</Button>
+            
+        </Box>
+    )
+}
+
+function EditableControls({ isEditing, onSubmit, onCancel, onEdit }) {
+    return isEditing ? (
+      <ButtonGroup justifyContent="center" size="sm">
+        <IconButton icon={<CheckIcon />} onClick={onSubmit} aria-label='submit'/>
+        <IconButton icon={<CloseIcon />} onClick={onCancel} aria-label='cancel'/>
+      </ButtonGroup>
+    ) : (
+      <Flex justifyContent="center">
+        <IconButton size="sm" icon={<EditIcon />} onClick={onEdit} aria-label='edit'/>
+      </Flex>
+    )
+  }
+
+
+function ChooseImage({ imageId }) {
+    return (
+    <Flex justifyContent="center">
+      <ButtonGroup justifyContent="center" size="sm">
+        <IconButton icon={<ChevronLeftIcon />} onClick={prevImage} isRound={true} aria-label='previous'/>
+        {render()}
+        <IconButton icon={<ChevronRightIcon />} onClick={nextImage} isRound={true} aria-label='next'/>
+        </ButtonGroup>
+    </Flex>
+    )
+    function prevImage() {
+        imageId--
+      }
+
+    function nextImage() {
+        imageId++
+      }
+
+    function render(){
+        return(
+        <Image
+            borderRadius="full"
+            boxSize="45px"
+            src={imageList[imageId]}
+            alt="Avatar"
+            />) }
+
+  }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,12 +8,13 @@ import {
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 import { Container } from '../components/Container'
-import { DiceRoller } from '../components/DiceRoller'
+//import { DiceRoller } from '../components/DiceRoller'
 //import { Home } from '../components/Home'
+import { ChooseCharacter } from '../components/ChooseCharacter'
 
 const Index = () => (
   <Container height="100vh">
-    <DiceRoller />
+    <ChooseCharacter />
   </Container>
 )
 
