cfd8b5d84bad09babc1a33aa7eff5ecf1f8253ee
huangnq96 <huangnq1996@gmail.com>
2021-03-08 13:24:16 +0800

    server chnages 1.0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,52 @@
-console.log('hello orld')
+import { MikroORM } from "@mikro-orm/core";
+//import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import "reflect-metadata";
+
+const main = async () => {
+  // connect to database
+  const orm = await MikroORM.init(microConfig);
+
+  // run migration
+  await orm.getMigrator().up();
+
+  // run sql
+
+  // upload things to sql
+  // const post1 = orm.em.create(Post, {title: 'my second post1'});
+  // await orm.em.persistAndFlush(post1);
+
+  // find things in sql
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em })
+  })
+
+  // app.get('/',(_, res) => {
+  //     res.send("hello");
+  // })
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+  })
+
+}
+
+main();
+
---
+++ server/src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default{
+    migrations: {
+        path: path.join(__dirname,'./migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    },
+    entities: [Post],
+    dbName: 'postgres',
+    type: 'postgresql',
+    password: 'postgres',
+    debug: !__prod__,
+  }as Parameters<typeof MikroORM.init>[0];
