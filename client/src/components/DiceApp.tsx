436502d5dc5f32d74458aa1642f1425103f26538
behweifeng <55077950+behweifeng@users.noreply.github.com>
2021-03-11 11:35:49 +0800

    Update DiceApp.tsx

--- client/src/components/DiceApp.tsx
+++ client/src/components/DiceApp.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Dice from "react-dice-roll";
 
-export default function DiceApp({ handlerKey }: { handlerKey: string }) {
+export default function DiceApp() {
   return (
     <div className="DiceApp">
       <Dice size={100} onRoll={(value) => console.log(value)} />
