01f539f3d714b7487b1860896408c8b84932d3b1
hung <hungdp1999@gmail.com>
2021-03-24 22:04:33 +0800

    Add css to character page

--- client/src/components/Character.tsx
+++ client/src/components/Character.tsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Box} from "@chakra-ui/react";
 
 
 export default class Character extends Component {
@@ -7,26 +8,40 @@ export default class Character extends Component {
     render(){
         return(
             <div>
-                <span>Player {this.props.character.characterId} coins: {this.props.character.playerCoins}</span>
-                <button
+                <Box color="black" bg="green.300" px={4} fontSize="30px"> 
+                    <b> Player  {this.props.character.characterId} </b>    
+                </Box>
+                <Box bg ="yellow" fontSize="25px" px={4}>
+                     Coins: {this.props.character.playerCoins}  
+                </Box>         
+                
+                <br></br>
+                <Box as="button" style={ButtonStyle}  px={4} mr="10px"
                 onClick={() => this.props.decreaseCoins(this.props.character)}
                 className="btn btn-secondary btn-sm"
                 >
                     Decrease
-                </button>
-                <button
+                </Box>
+                <Box as="button" style={ButtonStyle} px={4} mr="10px"
                 onClick={() => this.props.increaseCoins(this.props.character)}
                 className="btn btn-secondary btn-sm"
                 >
                     Increase
-                </button>
-                <button
+                </Box>
+                <Box as="button" style={ButtonStyle} px={4} mr="10px"
                 onClick={() => this.props.nextTurn(this.props.character)}
                 className="btn btn-secondary btn-sm"
                 >
                     nextTurn
-                </button>
+                </Box>
             </div>
         );
     }
+}
+
+const ButtonStyle = {
+    backgroundColor: "tomato",
+    color: "yellow",
+    borderRadius: "10px",
+    fontSize:"20px"
 }
