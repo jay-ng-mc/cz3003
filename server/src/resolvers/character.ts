8a8db8f4b0dc7c2e216772a157917eba7e7685bc
Jay <milkweedz@hotmail.com>
2021-04-19 01:00:05 +0800

    fix: create createCharacter query and invoke it (instead of updateCharacter) when register user

--- server/src/resolvers/character.ts
+++ server/src/resolvers/character.ts
@@ -1,6 +1,7 @@
 import { MyContext } from "src/types";
-import { Query, Arg, Ctx, Resolver, Mutation, InputType, Field } from "type-graphql";
+import { Query, Arg, Ctx, Resolver, Mutation, InputType, Field, ObjectType } from "type-graphql";
 import { Character } from "../entities/Character";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class CharacterInput {
@@ -8,6 +9,12 @@ class CharacterInput {
     username: string;
     @Field()
     characterId: number;
+}
+
+@ObjectType()
+class CharacterResponse {
+    @Field(() => Character, { nullable: true })
+    character?: Character
 }
 
 @Resolver()
@@ -21,20 +28,36 @@ export class CharacterResolver {
         return em.findOne(Character, { username });
     }
 
-    @Mutation(() => Character)
+    @Mutation(() => CharacterResponse)
+    async createCharacter(
+        @Arg('options') options: CharacterInput,
+        @Ctx() { em }: MyContext
+    ) {
+        var character
+        const result = await (em as EntityManager)
+            .createQueryBuilder(Character)
+            .getKnexQuery()
+            .insert({
+                username: options.username,
+                character_id: options.characterId
+            }).returning("*");
+        character = result[0]
+    }
+
+    @Mutation(() => CharacterResponse)
     async updateCharacter(
         @Arg('options') options: CharacterInput,
         @Ctx() { em }: MyContext
-    ): Promise<Character | null> {
-        const character = await em.findOne(Character, { username: options.username })
+    ): Promise<CharacterResponse | null> {
+        var character
+        character = await em.findOne(Character, { username: options.username })
         if (character != null){
             character.characterId = options.characterId;
-            
             await em.persistAndFlush(character);
         } else{
             return null
         }
-        return character;
+        return {character}
     }
 
 }
