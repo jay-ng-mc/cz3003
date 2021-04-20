8ac085d0764f2b2149f8cb8bc7debca212f5a51b
Jay <milkweedz@hotmail.com>
2021-04-20 10:04:10 +0800

    fix: pass all usernames into board when game lobby starts game

--- client/src/components/GameLobby.tsx
+++ client/src/components/GameLobby.tsx
@@ -13,7 +13,7 @@ import Modal from 'react-awesome-modal';
 import Sublogin from './Sublogin';
 import { useMeQuery, useGetCharacterQuery } from "../generated/graphql";
 
-let imageList = ["images\\p0.png","images\\p1.png","images\\p2.png","images\\p3.png","images\\p4.png","images\\p5.png"]
+let imageList = ["icons\\1.png","icons\\2.png","icons\\3.png","icons\\4.png","icons\\5.png", "icons\\6.png", "icons\\7.png", "icons\\8.png"]
 
 const GameLobby = (props) => {
     return (
@@ -60,7 +60,7 @@ const GameLobbyBox = (props) => {
                         <Header/>
                     </Box>
                     <Box flex='3'>
-                        <GameLobbyContent updateState={props.updateState} nextView={props.nextView} username={userName} imageId={savedImageIdx}/>
+                        <GameLobbyContent updateState={props.updateState} nextView={props.nextView} userName={userName} imageId={savedImageIdx}/>
                     </Box>
                 </Box>
             </Box>
@@ -115,7 +115,7 @@ const DispUsers = (props) => {
 
     if (props.num < props.userList.length){
         output = <Box w="100%" h="200" bg="gray.200" textAlign='right'>        
-            <IconButton aria-label="Delete Character" isRound={true} icon={<CloseIcon />} onClick={() => props.removeUser(props.num)}  size='md'/>&#8239;
+            <IconButton isDisabled={props.removeUser == null} aria-label="Delete Character" isRound={true} icon={<CloseIcon />} onClick={() => props.removeUser(props.num)}  size='md'/>&#8239;
             <Center><Image borderRadius="full" boxSize="30px" src={imageList[ImageId(props.userList[props.num])]} alt="Avatar" img id="Avatar" /></Center>
             <Center>{props.userList[props.num]}</Center>
         </Box>
@@ -169,11 +169,11 @@ class Popup extends Component<{addUser}> {
     }
 }
 
-class GameLobbyContent extends React.Component<{username, imageId}>{
+class GameLobbyContent extends React.Component<{userName, imageId}>{
     constructor(props){
         super(props)
         this.state = {
-            users: []
+            users: [] // init with logged in user
         }
         this.addUser = this.addUser.bind(this)
         this.removeUser = this.removeUser.bind(this)
@@ -188,25 +188,32 @@ class GameLobbyContent extends React.Component<{username, imageId}>{
         this.setState({
             ...this.state.users.splice(indexNum,1)
         })
-    } 
+    }
+    
+    componentDidUpdate(){
+        if (this.props.userName && this.state.users.indexOf(this.props.userName) == -1) {
+            this.addUser(this.props.userName)
+        }
+    }
 
     render(){
         return(
             <Box my={5} textAlign='left'>
                 <div>
                     <Grid templateColumns="repeat(4, 1fr)" gap={1}>
-                        <Box w="100%" h="200" bg="gray.200" textAlign='center'>        
+                        {/* <Box w="100%" h="200" bg="gray.200" textAlign='center'>        
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             <Center><br/><br/><Image borderRadius="full" boxSize="30px" src={imageList[this.props.imageId]} alt="Avatar" img id="Avatar" /></Center>
-                            {this.props.username}
-                        </Box>
-                        < DispUsers num={0} userList={this.state.users} addUser={this.addUser} removeUser={this.removeUser} />
+                            {this.props.userName}
+                        </Box> */}
+                        < DispUsers num={0} userList={this.state.users} addUser={this.addUser} removeUser={null}/>
                         < DispUsers num={1} userList={this.state.users} addUser={this.addUser} removeUser={this.removeUser} />
                         < DispUsers num={2} userList={this.state.users} addUser={this.addUser} removeUser={this.removeUser} />
+                        < DispUsers num={3} userList={this.state.users} addUser={this.addUser} removeUser={this.removeUser} />
                         
                     </Grid>
                     <Button onClick={() => {
-                        this.props.updateState({users: this.state.users}) 
+                        this.props.updateState({users: this.state.users})
                         this.props.nextView()
                     }} width='full' mt={5}>New Game</Button>
                 </div>
